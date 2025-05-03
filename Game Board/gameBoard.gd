f136591ad8866504cf54bbb5f78dd784fcfd2110
Ian Roller <ianroller4@gmail.com>
2025-05-03 16:20:25 -0500

    Levels

--- Game Board/gameBoard.gd
+++ Game Board/gameBoard.gd
@@ -26,15 +26,17 @@ var fastFallMult := 10.0
 
 var isGameRunning := true
 
+var totalLinesCleared := 0
+var level := 1
+
 func _ready():
 	start_game()
 
 func _physics_process(delta):
 	if isGameRunning:
 		var currentFallInterval = fallInterval
-		if Input.is_action_just_pressed("ui_down"):
+		if Input.is_action_just_pressed("down"):
 			currentFallInterval /= fastFallMult
-		
 		fallTimer += delta
 		if fallTimer >= currentFallInterval:
 			move_block(Vector2.DOWN)
@@ -46,10 +48,6 @@ func _input(event):
 			move_block(Vector2.LEFT)
 		if event.is_action_pressed("right"):
 			move_block(Vector2.RIGHT)
-		if event.is_action_pressed("down"):
-			move_block(Vector2.DOWN)
-		if event.is_action_pressed("hard_drop"):
-			hard_drop()
 		if event.is_action_pressed("rotate_left"):
 			rotate_left()
 		if event.is_action_pressed("rotate_right"):
@@ -140,9 +138,6 @@ func set_board_cells():
 	for coor in tileCoordinates[rotationIndex]:
 		BOARD.set_cell(currentPosition + coor, Global.BLOCK_TILE_IDS[currentBlockType], Vector2(0, 0))
 
-func hard_drop():
-	pass
-
 func rotate_left():
 	if is_valid_rotation_left():
 		clear_block()
@@ -186,9 +181,18 @@ func check_rows():
 		if filledCells == COLS:
 			shift_rows(rows)
 			clearCount += 1
+			totalLinesCleared += 1
+			update_level()
 		else:
 			rows -= 1
 	update_score(clearCount)
+
+func update_level():
+	var newLevel = ceil(float(totalLinesCleared) / 10.0)
+	if newLevel > level:
+		level = newLevel
+		fallInterval *= 0.9
+		$VBoxContainer/Level.text = str(level)
 
 func shift_rows(row : int):
 	var tile
@@ -203,13 +207,13 @@ func shift_rows(row : int):
 func update_score(rowsCleared : int):
 	match(rowsCleared):
 		1:
-			Score.currentScore += 100
+			Score.currentScore += 100 * level
 		2:
-			Score.currentScore += 300
+			Score.currentScore += 300 * level
 		3:
-			Score.currentScore += 500
+			Score.currentScore += 500 * level
 		4:
-			Score.currentScore += 800
+			Score.currentScore += 800 * level
 		_:
 			pass
 	SCORE.text = str(Score.currentScore)
